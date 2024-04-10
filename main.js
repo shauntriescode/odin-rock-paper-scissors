b1fbcd700f0d13d9960023b630d65e182f187b09
Shaun <shaunbrianwilsonventures@gmail.com>
2024-04-10 14:43:52 +0100

    commit two

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Reset on page load
+let playerSelection = null;
 // Step 1 - getComputerChoice
 
 // Function with a array to get computer answer
@@ -39,19 +41,87 @@ function singleRound(playerSelection, computerSelection) {
         return `Please enter a valid input`
       }
     };
-    
 
-// Step 3 - Into a for loop to repeat 5 times
+
+// // Step 3 - Into a for loop to repeat 5 times
   
 function playGame() {
-  // End at 4 due to i beginning with 0
-  for (i = 0; i <= 4; i++) {
-
-  const playerSelection = prompt('Rock, Paper or Scissors?')
+  if (playerSelection !== null) {
   const computerSelection = getComputerChoice()
   const roundResult = singleRound(playerSelection, computerSelection)
-  console.log(roundResult);
+  updateResult(roundResult);
+  score(roundResult);
+}};
+
+playGame();
+
+
+// Event Listeners
+
+// Rock
+const rock = document.querySelector('#rock')
+
+rock.addEventListener('click', function() {
+  playerSelection = 'rock'
+  playGame()
+});
+
+// Paper
+const paper = document.querySelector('#paper');
+
+paper.addEventListener('click', function() {
+  playerSelection = 'paper'
+  playGame()
+});
+
+// Scissors
+const scissors = document.querySelector('#scissors')
+scissors.addEventListener('click', function() {
+  playerSelection = 'scissors'
+  playGame()
+});
+
+// Result
+
+result = document.querySelector('#result');
+
+function updateResult(roundResult) {
+  result.textContent = `${roundResult} player score: ${playerScore}, computer score: ${computerScore}`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    endGame()
   }
 };
 
-playGame();
+// Score
+
+let playerScore = 0;
+let computerScore = 0;
+
+function score(roundResult) {
+  if (roundResult.includes('win')) {
+    playerScore++
+  } else if (roundResult.includes('lose')) {
+    computerScore++
+  } else {
+    return 'draw'
+  }
+};
+
+// End game and reset to 0
+
+function endGame() {
+  if (playerScore === 5) {
+    alert('congratulations, you win!')
+    playerScore = 0;
+    computerScore = 0;
+  } else {
+    alert('game over! better luck next time');
+    playerScore = 0;
+    computerScore = 0;
+  } 
+};
+
+
+
+
